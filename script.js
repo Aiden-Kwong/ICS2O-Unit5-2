6040faac59017e9ef8333b32b61bd00593694aa4
AidenKwong <>
2021-06-01 15:17:46 +0000

    added comments

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// This checks for if the button is clicked and if it is, it triggers the function called message.
 document.getElementById('button').addEventListener('click', message)
 
+// This sends the alert popup when the function is triggered (by the button).
 function message () {
   alert('Button was clicked')
 }
